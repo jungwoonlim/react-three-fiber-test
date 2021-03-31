5d2f116235743426178cd36ef26d537649393220
jungwoonlim <wypo23@naver.com>
2021-03-31 23:27:32 +0900

    기본 샘플 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+import FirstSample from "./FirstSample";
+
 function App() {
   return (
     <div className="App">
       <h1>Learn react-three-fiber</h1>
+      <FirstSample />
     </div>
   );
 }
